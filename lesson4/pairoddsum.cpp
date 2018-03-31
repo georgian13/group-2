ba778c1f9402dbb6045c63dc34bf3a6b958edbba
Hrach Gevorgyan <hrgeorgian13@gmail.com>
2018-03-31 19:28:29 +0400

    C++ Works (homework)

--- lesson4/pairoddsum.cpp
+++ lesson4/pairoddsum.cpp
@@ -4,19 +4,17 @@ unsigned int pair_sum(int a) {
         if ( a % 2 == 0 ) {
             return a + pair_sum(a-2);
         } else {
-            a--;
-            pair_sum(a);
+            pair_sum(--a);
         }
     }
 }
 
 unsigned int odd_sum(int a) {
-    if ( a > 0 ) {
+    if ( a > 1 ) {
         if ( a % 2 == 1 ) {
             return a + odd_sum(a-2);
         } else {
-            a--;
-            odd_sum(a);
+            odd_sum(--a);
         }
     }
 }
@@ -29,7 +27,7 @@ int main() {
         pair_sum(b);
         std::cout << "1 - " << b << " pair summa equale " << pair_sum(b) << std::endl;
         odd_sum(b);
-        std::cout << "1 - " << b << "  odd summa equale " << 1 + odd_sum(b) << std::endl;
+        std::cout << "1 - " << b << "  odd summa equale " << odd_sum(b) << std::endl;
     } else {
         std::cout << "This number is signed or zero \n" ;
     }
