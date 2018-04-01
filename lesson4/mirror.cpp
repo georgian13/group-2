2c93445b40e884b90ba6bbee9bc91c24e030e039
Hrach Gevorgyan <hrgeorgian13@gmail.com>
2018-04-01 13:14:16 +0400

    C++ Works (homework)

--- lesson4/mirror.cpp
+++ lesson4/mirror.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-unsigned int mirror(int a) {
+unsigned int mirror(int a,int n) {
     if ( a != 0 ) {
-        int n;
-        n = a % 10;
-        std::cout << n <<" ";
+        n = 10*n + a % 10;
         a = a / 10;
-    return mirror(a);
+    return mirror(a,n);
+    } else {
+        return n;
     }
 }
 
 int main() {
-    int b;
+    int b, k = 0;
     std::cout << "Enter unsigned number \n";
     std::cin >> b;
     if ( b > 0 ) {
-        std::cout << "Mirror number => " << mirror(b) << std::endl;
+        std::cout << "Mirror number => " << mirror(b,k) << std::endl;
     } else {
         std::cout << "This number is signed or zero \n" ;
     }
