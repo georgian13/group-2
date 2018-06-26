79b4582a95c0be62b713335fda7cffe13228667b
Hrach Gevorgyan <hrgeorgian13@gmail.com>
2018-06-26 11:02:27 +0400

    Modified Homeworks 26.06

--- HTML/CV_script/script.js
+++ HTML/CV_script/script.js
@@ -1,7 +1,28 @@
-jQuery(document).ready(function($) {
-    $("#myModal").modal('show');
-  });
-var count = 0;
+jQuery(document).ready(function($)  {
+    if (sessionStorage.getItem("name") == null) {
+        $("#myModal").modal("show");
+    } else {
+        loading();
+    }
+    if (sessionStorage.getItem("key") != null) {
+        var res = JSON.parse(sessionStorage["key"]);
+        $("#resultBox").html(res);
+    }
+    $("#myModal").submit(function() {
+        if ($("#gender_Male").is(":checked")) {
+            $("#modal").html("Misis");
+        }
+
+        if ($("#gender_Female").is(":checked")) {
+            $("#modal").html("Misis");
+        }
+
+        push();
+        saveing();
+        $("#myModal").modal("hide");
+        return false;
+    });
+});
 function push() {  
     if(document.getElementById('gender_Male').checked) {
         $("#h1").html("Mister");
@@ -27,18 +48,53 @@ function addinfo() {
         content += "</table>"
         $('#area').append(content); 
     $("#form2")[0].reset();
+ }
+   
+ 
+ function loading() {
+    var name = sessionStorage.getItem("name1");
+    var surname = sessionStorage.getItem("surname1")
+
+    if (sessionStorage.getItem("gender") == "Male") {
+        $("#modal").html("Mister ");
     }
-window.onload = function saveSettings() {
-    var name = document.getElementById("name1").value;
-    var surname = document.getElementById("surname1").value;
-    var gender;
-    if (document.getElementById("surname1").checked) {
-        gender = "Mister";
-    } else if(document.getElementById("surname1").checked) {
-        gender = "Misis";
+    if (sessionStorage.getItem("gender") == "Female") {
+        $("#modal").html("Misis ");
     }
-    ​var test = { name: name, surname: "thing2", test3: [0, 2, 44] }​​​​​​​;
-    localStorage.setItem("test", JSON.stringify(test));
-    var test2 = localStorage.getItem("test");
-    test = JSON.parse(test2);
+
+    $("#mame1").text(name);
+    $("surname1").text(surname);
 }
+
+function saveing() {
+     var gender = "";
+
+    if ($("#gender_Male").is(":checked")) {
+        gender = "Male";
+    }
+    if ($("#gender_Female").is(":checked")) {
+        gender = "Female";
+    }
+
+    sessionStorage.setItem("name", $("#name1").val());
+    sessionStorage.setItem("surname", $("#surname1").val());
+    sessionStorage.setItem("gender", gender);
+    loading();
+}
+function resultsave() {
+    var json1 = JSON.stringify($("#area").html());
+    sessionStorage.setItem("key", json1);
+}
+
+
+
+
+
+
+    
+
+        
+       
+
+        
+       
