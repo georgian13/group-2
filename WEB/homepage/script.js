35af17d357cad7f75a69eb3951e71f3772fbf62a
Ani <ani_hakobyan_98@bk.ru>
2018-06-28 18:07:44 +0400

    added modified Home Page

--- WEB/homepage/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#myBtn").click(function(){
-        $("#myModal").modal();
-    });
-    $("#mySign").click(function(){
-        $("#SignUp").modal();
-    });
-});
